9e87b004f48bdb7727525efdf886004b6e81f16c
raspberry <1659514328@qq.com>
2015-01-07 16:55:59 +0000

    test

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask
-from flask import request
-
+from flask import Flask, request, render_template
+from rgb import rgb
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-
 def home():
-	return '<h1>Home</h1>'
+	return render_template('index.html')
 
-@app.route('/signin', methods=['GET'])
-def signin_form():
+@app.route('/rgb.html', methods=['GET'])
+def rgb():
+	#print request.form['red']
 	return '''<form action="/signin" method="post">
-		<p><input name="username"></p>
-		<p><input name="password" type="password"></p>
-		<p><button type="submit">Sign In</button></p>
-		</form>'''
+	              <p><input name="username"></p>
+		                    <p><input name="password" type="password"></p>
+				                  <p><button type="submit">Sign In</button></p>
+						                </form>'''
 
 @app.route('/signin', methods=['POST'])
 def signin():
 	if request.form['username']=='admin' and request.form['password']=='password':
+		rgb(1,1,1)
 		return '<h3>Hello, admin!</h3>'
 	return '<h3>Bad username or password</h3>'
 
