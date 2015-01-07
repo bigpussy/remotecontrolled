b70d1e752357ebde11849da078072b7a9f2cf115
raspberry <1659514328@qq.com>
2015-01-07 15:21:50 +0000

    test

--- rgb.py
+++ rgb.py
@@ -2,5 +2,9 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
 def rgb(red,green,blue):
 	GPIO.output(11, red)
+	GPIO.output(12, green)
+	GPIO.output(13, blue)
